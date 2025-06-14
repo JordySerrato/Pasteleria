59d51cbfe748ea4184914d407155119b14a20dd4
JordySerrato <samuel_14_marzo@hotmail.com>
2025-06-13 23:53:45 -0500

    1
    1

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -5,21 +5,41 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          //icono
-          Icon(Icons.lock), // <--- SOLUCIÓN: Cambiado a Icons.lock
-          //textfield usuario
+      backgroundColor: const Color.fromARGB(255, 0, 217, 255),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+              //icono
+              Icon(Icons.lock, size: 100, color: Colors.red,), // <--- SOLUCIÓN: Cambiado a Icons.lock
+              SizedBox(height: 20,),
+              Text("login",style: TextStyle(fontSize: 30 ,color: Colors.black, fontWeight: FontWeight.bold),),
+              //textfield usuario
+              
+              SizedBox(height: 20,),
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.green))
 
-          //textfield pass
+                  ),
+                ),
+              )
 
-          //button
-
-          //register
-
-          //forgot password
-
-        ],
+              //textfield pass
+          
+              //button
+          
+              //register
+          
+              //forgot password
+          
+            ],
+          ),
+        ),
       ),
     );
   }
